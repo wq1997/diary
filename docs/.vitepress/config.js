96a7b54669460ead419361778c0a88c73a472a12
Qing Wang <qing.wang@basebit.ai>
2022-09-29 15:59:36 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: '王清diao的一逼',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'React',
+        items: [
+          { text: 'Introduction', link: '/react/introduction' },
+          { text: 'Getting Started', link: '/react/getting-started' },
+        ]
+      }
+    ]
+  }
+}
