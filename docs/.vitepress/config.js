26f520d3e4255cc3884dccc6a737b3328a0f4007
Qing Wang <qing.wang@basebit.ai>
2022-09-29 16:05:59 +0800

    增加常用命令

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -8,6 +8,12 @@ module.exports = {
           { text: 'Introduction', link: '/react/introduction' },
           { text: 'Getting Started', link: '/react/getting-started' },
         ]
+      },
+      {
+        text: '笔记',
+        items: [
+          { text: '常用命令', link: '/notes/1' },
+        ]
       }
     ]
   }
--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,3 @@
 # Hello VitePress
 
-sdfsalghlseg
+Hello
